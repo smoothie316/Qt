4b0bdd7f3f32de858bd83a02204ae669fb6cc185
gch04721 <jwhuh@uclab.re.kr>
2019-03-27 18:13:11 +0900

    chagne MainTab, SubTab ratio

--- FileIO/fileclass.cpp
+++ FileIO/fileclass.cpp
@@ -10,6 +10,7 @@ FileClass::FileClass(QWidget *parent) : QPushButton(parent)
 void FileClass::clicked(){
     if (this->objectName() == QString("FileOpen")){
         // 파일 오픈 코드. 사용가능한 확장자 지정 필요(strFilter).
+        // jpg, jpeg, png, bmp
         // 파일 복수선택 가능하게 만들기
         QString strFilter = "All files (*.*)";
         QString tmpStr = QFileDialog::getOpenFileName(this, "Image", QDir::homePath(), strFilter);
