1485feb53b3b5f41ad4a17ef2e439ed5016ca232
gch04721 <jwhuh@uclab.re.kr>
2019-04-07 17:08:37 +0900

    remove QDockWidget

--- ToolBtn/toolbtn.cpp
+++ ToolBtn/toolbtn.cpp
@@ -17,33 +17,21 @@ void ToolBtn::clicked(){
     switch(this->getNumber(this->objectName())){
         case 1:
         {
-            QDockWidget* dock = new QDockWidget("Brush Set");
-            dock->setWidget(brushW);
-            dock->setFloating(true);
-            dock->show();
+            brushW->show();
             break;
         }
         case 2:
         {
-            QDockWidget* dock1 = new QDockWidget("Paint Set");
-            dock1->setWidget(paintW);
-            dock1->setFloating(true);
-            dock1->show();
+            paintW->show();
             break;
         }
         case 3:
         {
-            QDockWidget* dock2 = new QDockWidget("Text Set");
-            dock2->setWidget(textW);
-            dock2->setFloating(true);
-            dock2->show();
+            textW->show();
             break;
         }
         case 4:{
-            QDockWidget* dock3 = new QDockWidget("Erase Set");
-            dock3->setWidget(EraseW);
-            dock3->setFloating(true);
-            dock3->show();
+            EraseW->show();
             break;
         }
     }
